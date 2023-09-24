95b7c5897fc6f85d49bb7f5e3d92d38fe71616ea
liu <liu@huawei>
2023-09-24 22:31:28 +0800

    增加了管理员提示

--- fix.bat
+++ fix.bat
@@ -1,13 +1,21 @@
 @echo off
-echo turn off quick edit mode for cmd.exe
+
+NET SESSION >NUL 2>NUL
+if %errorlevel% neq 0 (
+    echo please run as administrator
+    pause
+    exit /b
+)
+
+echo turn off quick edit mode for cmd.exe ...
 reg add HKCU\Console /v QuickEdit /t REG_DWORD /d 0 /f
 
-echo install handwriting
+echo install handwriting ...
 setlocal
 for %%A in ("%~dp0\*.cab") do (
     dism /Online /Add-Package /PackagePath:"%%A"
 )
 endlocal
 
-echo modify non-unicode language
+echo modify non-unicode language ...
 Intl.cpl
