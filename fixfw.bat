152a1ed553006adbb7beb095f98978448aea982e
liu <liu@book16>
2024-03-25 10:13:44 +0800

    不再允许rdp

--- fixfw.bat
+++ fixfw.bat
@@ -17,7 +17,7 @@ IF %ERRORLEVEL% EQU 0 (
 @REM 加速psexec的调用 https://serverfault.com/a/805853
 netsh advfirewall firewall set rule name="Remote Service Management (RPC)" profile=private,public new enable=yes
 
-call :AddFirewallRule "xw_RDPA" "TCP" 3389
+@REM call :AddFirewallRule "xw_RDPA" "TCP" 3389
 call :AddFirewallRule "xw_TwinCAT_TCP" "TCP" 48898
 call :AddFirewallRule "xw_TwinCAT_UDP" "UDP" 48898
 call :AddFirewallRule "xw_TwinCAT_DISCOVERY_UDP" "UDP" 48899
